5913dcf56e518da9cc2ca89cc19b37fe8cf320d8
Alex <alexomelyanovich7@gmail.com>
2025-11-07 23:11:02 +0300

    Первая версия проекта от Алексея

---
+++ src/test/java/xack/vtb/VtbApplicationTests.java
@@ -0,0 +1,13 @@
+package xack.vtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
