38ba070ce3fcec55965abe06c500564c5519e90c
alexomelyanovich7 <alexomelyanovich7@gmail.com>
2025-11-08 22:00:17 +0300

    Create OrchestraApplication.java

---
+++ OrchestraApplication.java
@@ -0,0 +1,11 @@
+package com.vtb.hackathon.orchestra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestraApplication.class, args);
+    }
+}
