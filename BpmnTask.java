a504c69dcf91ce72f741e3e0f0ff143a6c65a751
alexomelyanovich7 <alexomelyanovich7@gmail.com>
2025-11-08 21:58:54 +0300

    Create BpmnTask.java

---
+++ BpmnTask.java
@@ -0,0 +1,17 @@
+package com.vtb.hackathon.orchestra;
+
+public class BpmnTask {
+    private String name;
+    
+    public BpmnTask(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
